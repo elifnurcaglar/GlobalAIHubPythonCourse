74a314f29bcd94f544f969cc6f0c452b2d66aebc
Elif Nur Caglar <60555133+elifnurcaglar@users.noreply.github.com>
2021-02-04 17:25:03 +0300

    Add files via upload

---
+++ Homeworks/hw2b.py
@@ -0,0 +1,16 @@
+print("****login****")
+
+login = {"userEmail": "elifn", "userPassword": "trytrytry"}
+
+logEmail = input("Please enter your email:")
+logPassword = input("Please enter your pasword:")
+
+
+if (login["userEmail"] != logEmail and login["userPassword"] != logPassword):
+    print("Invalid email and password.")
+elif (login["userEmail"] != logEmail and login["userPassword"] == logPassword):
+    print("Invalid email.")
+elif (login["userEmail"] == logEmail and login["userPassword"] != logPassword):
+    print("Invalid password")
+else:
+    print("Logged in successfully.")
