18e6eab92179e1f2e29cffcfd6a5f66dfcfd9df2
Elif Nur Caglar <60555133+elifnurcaglar@users.noreply.github.com>
2021-02-04 16:42:41 +0300

    Add files via upload

---
+++ Homeworks/hw1a.py
@@ -0,0 +1,8 @@
+A = [1,2,3,4,5,6]
+B = A[:len(A)//2]
+C = A[len(A)//2:]
+print(A)
+print(B)
+print(C)
+reverseA = C + B
+print(reverseA)
---
+++ Homeworks/hw2a.py
@@ -0,0 +1,17 @@
+print("******login******")
+userEmail = "elif"
+userPassword = "trytrytry"
+
+logEmail = input("Please enter your email:")
+logPassword = input("Please enter your password:")
+
+if (userEmail == logEmail):
+    if (userPassword == logPassword):
+        print("Logged in successfully")
+    else:
+        print("Wrong password")
+elif (userEmail != logEmail and userPassword != logPassword):
+    print("Wrong email and password")
+else:
+    print("Wrong email")
+    
