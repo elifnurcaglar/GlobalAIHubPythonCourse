98f50743679e416995e1bf3b386ef4c6deb4143b
Elif Nur Caglar <60555133+elifnurcaglar@users.noreply.github.com>
2021-02-05 16:53:12 +0300

    Update hw3.py

--- Homeworks/hw3.py
+++ Homeworks/hw3.py
@@ -1,7 +1,4 @@
 def primenumber(x, y):
-    for b in range(x ,y):
-        if x == 0:
-            continue
     primenumberList = [i for i in range(2, y + 1) if all(i % j for j in range(2, i))]
     print(primenumberList)
 
